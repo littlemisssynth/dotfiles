78f946e0c772c638dbd86995928bf41ac2f48d38
Leah Nicole <51840792+littlemisssynth@users.noreply.github.com>
2023-01-09 18:12:41 +0000

    Create preinstall.sh

---
+++ preinstall.sh
@@ -0,0 +1,11 @@
+if [[ -f /usr/bin/pacman ]]; then
+    sudo pacman -Syu --needed flatpak
+            flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    sudo reboot
+elif [[ -f /usr/bin/apt ]]; then
+    sudo apt install flatpak
+            flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    sudo reboot
+else
+    echo "nothing to presetup"
+fi
