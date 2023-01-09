46182c3b9f6db51fdc7e34753c484af63568c23a
Leah Nicole <51840792+littlemisssynth@users.noreply.github.com>
2023-01-09 18:18:45 +0000

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -3,6 +3,7 @@ if [[ -f /usr/bin/pacman ]]; then
             flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
     sudo reboot
 elif [[ -f /usr/bin/apt ]]; then
+    sudo apt update
     sudo apt install flatpak
             flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
     sudo reboot
