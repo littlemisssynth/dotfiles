4dd504e59b079405456fb0746837357650a767b8
Leah Nicole <51840792+littlemisssynth@users.noreply.github.com>
2023-11-27 03:31:03 +0000

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -4,6 +4,7 @@ if [[ -f /usr/bin/pacman ]]; then
     sudo reboot
 elif [[ -f /usr/bin/apt ]]; then
     sudo apt update
+    sudo dpkg --add-architecture i386
     sudo apt install flatpak
             flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
     sudo reboot
